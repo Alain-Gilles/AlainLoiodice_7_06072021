a9e713768a671d042c6bfe45b1193e3e3407690f
Alain-Gilles <76204103+Alain-Gilles@users.noreply.github.com>
2021-07-27 14:07:11 +0200

    Groupomania

--- SVroutes/routes/Gestuser.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userCtrl = require("../controllers/Gestuser");
-const auth = require("../middleware/auth");
-
-router.delete("/", auth, userCtrl.deleteUser);
-
-module.exports = router;
